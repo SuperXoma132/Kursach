83ecdf026062f44c98493abca3d5070120a52389
SuperXoma132 <superxoma132@gmail.com>
2025-01-23 01:59:26 +0300

    Enter -> Entrance

--- Курсовая работа БОЯП/Курсовая работа БОЯП/Entrance.Designer.cs
+++ Курсовая работа БОЯП/Курсовая работа БОЯП/Entrance.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Курсовая_работа_БОЯП
 {
-    partial class Enter
+    partial class Entrance
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,48 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Enter));
             tabControl1 = new TabControl();
             Auth = new TabPage();
             label2 = new Label();
             label1 = new Label();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
-            button1 = new Button();
+            authPasswordBox = new TextBox();
+            authLoginBox = new TextBox();
+            authButton = new Button();
             tabPage2 = new TabPage();
-            pictureBox1 = new PictureBox();
             label3 = new Label();
             label4 = new Label();
-            textBox4 = new TextBox();
-            textBox3 = new TextBox();
-            button2 = new Button();
+            regPasswordBox = new TextBox();
+            regLoginBox = new TextBox();
+            regButton = new Button();
             tabControl1.SuspendLayout();
             Auth.SuspendLayout();
             tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(Auth);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(12, 15);
+            tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(535, 413);
+            tabControl1.Size = new Size(401, 303);
             tabControl1.TabIndex = 2;
             // 
             // Auth
             // 
             Auth.Controls.Add(label2);
             Auth.Controls.Add(label1);
-            Auth.Controls.Add(textBox2);
-            Auth.Controls.Add(textBox1);
-            Auth.Controls.Add(button1);
+            Auth.Controls.Add(authPasswordBox);
+            Auth.Controls.Add(authLoginBox);
+            Auth.Controls.Add(authButton);
             Auth.Location = new Point(4, 24);
             Auth.Name = "Auth";
             Auth.Padding = new Padding(3);
-            Auth.Size = new Size(527, 385);
+            Auth.Size = new Size(393, 275);
             Auth.TabIndex = 0;
             Auth.Text = "Авторизация";
             Auth.UseVisualStyleBackColor = true;
@@ -77,7 +74,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(160, 168);
+            label2.Location = new Point(101, 107);
             label2.Name = "label2";
             label2.Size = new Size(49, 15);
             label2.TabIndex = 4;
@@ -86,65 +83,55 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(160, 87);
+            label1.Location = new Point(101, 26);
             label1.Name = "label1";
             label1.Size = new Size(41, 15);
             label1.TabIndex = 3;
             label1.Text = "Логин";
             // 
-            // textBox2
+            // authPasswordBox
             // 
-            textBox2.Location = new Point(160, 186);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(176, 23);
-            textBox2.TabIndex = 2;
+            authPasswordBox.Location = new Point(101, 125);
+            authPasswordBox.Name = "authPasswordBox";
+            authPasswordBox.Size = new Size(176, 23);
+            authPasswordBox.TabIndex = 2;
             // 
-            // textBox1
+            // authLoginBox
             // 
-            textBox1.Location = new Point(160, 105);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(176, 23);
-            textBox1.TabIndex = 1;
+            authLoginBox.Location = new Point(101, 44);
+            authLoginBox.Name = "authLoginBox";
+            authLoginBox.Size = new Size(176, 23);
+            authLoginBox.TabIndex = 1;
             // 
-            // button1
+            // authButton
             // 
-            button1.Location = new Point(189, 267);
-            button1.Name = "button1";
-            button1.Size = new Size(117, 43);
-            button1.TabIndex = 0;
-            button1.Text = "Вход";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            authButton.Location = new Point(130, 206);
+            authButton.Name = "authButton";
+            authButton.Size = new Size(117, 43);
+            authButton.TabIndex = 0;
+            authButton.Text = "Авторизация";
+            authButton.UseVisualStyleBackColor = true;
+            authButton.Click += authButton_Click;
             // 
             // tabPage2
             // 
-            tabPage2.Controls.Add(pictureBox1);
             tabPage2.Controls.Add(label3);
             tabPage2.Controls.Add(label4);
-            tabPage2.Controls.Add(textBox4);
-            tabPage2.Controls.Add(textBox3);
-            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(regPasswordBox);
+            tabPage2.Controls.Add(regLoginBox);
+            tabPage2.Controls.Add(regButton);
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(527, 385);
+            tabPage2.Size = new Size(393, 275);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Регистрация";
             tabPage2.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(31, 191);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(465, 96);
-            pictureBox1.TabIndex = 10;
-            pictureBox1.TabStop = false;
-            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(168, 144);
+            label3.Location = new Point(101, 126);
             label3.Name = "label3";
             label3.Size = new Size(49, 15);
             label3.TabIndex = 9;
@@ -153,44 +140,44 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(168, 74);
+            label4.Location = new Point(101, 56);
             label4.Name = "label4";
             label4.Size = new Size(41, 15);
             label4.TabIndex = 8;
             label4.Text = "Логин";
             // 
-            // textBox4
+            // regPasswordBox
             // 
-            textBox4.Location = new Point(168, 162);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(176, 23);
-            textBox4.TabIndex = 7;
+            regPasswordBox.Location = new Point(101, 144);
+            regPasswordBox.Name = "regPasswordBox";
+            regPasswordBox.Size = new Size(176, 23);
+            regPasswordBox.TabIndex = 7;
             // 
-            // textBox3
+            // regLoginBox
             // 
-            textBox3.Location = new Point(168, 92);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(176, 23);
-            textBox3.TabIndex = 6;
+            regLoginBox.Location = new Point(101, 74);
+            regLoginBox.Name = "regLoginBox";
+            regLoginBox.Size = new Size(176, 23);
+            regLoginBox.TabIndex = 6;
             // 
-            // button2
+            // regButton
             // 
-            button2.Location = new Point(199, 293);
-            button2.Name = "button2";
-            button2.Size = new Size(117, 43);
-            button2.TabIndex = 5;
-            button2.Text = "Регистрация";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            regButton.Location = new Point(131, 191);
+            regButton.Name = "regButton";
+            regButton.Size = new Size(117, 43);
+            regButton.TabIndex = 5;
+            regButton.Text = "Регистрация";
+            regButton.UseVisualStyleBackColor = true;
+            regButton.Click += regButton_Click;
             // 
-            // Enter
+            // Entrance
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(543, 440);
+            ClientSize = new Size(420, 315);
             Controls.Add(tabControl1);
-            Name = "Enter";
-            Text = "Welcome";
+            Name = "Entrance";
+            Text = "Казаков Даниил ЭПИ-211";
             FormClosing += Enter_FormClosing;
             Load += Enter_Load;
             tabControl1.ResumeLayout(false);
@@ -198,7 +185,6 @@
             Auth.PerformLayout();
             tabPage2.ResumeLayout(false);
             tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -207,16 +193,15 @@
         private TabControl tabControl1;
         private TabPage Auth;
         private TabPage tabPage2;
-        private Button button1;
+        private Button authButton;
         private Label label2;
         private Label label1;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox authPasswordBox;
+        private TextBox authLoginBox;
         private Label label3;
         private Label label4;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private Button button2;
-        private PictureBox pictureBox1;
+        private TextBox regPasswordBox;
+        private TextBox regLoginBox;
+        private Button regButton;
     }
 }
